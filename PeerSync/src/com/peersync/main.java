1372ee2c23982c7ba55978029089246aedf72f73
Nicolas Leleu <n.leleu@hotmail.fr>
2013-06-15 15:03:17 +0200

    test upnp

--- PeerSync/src/com/peersync/main.java
+++ PeerSync/src/com/peersync/main.java
@@ -4,8 +4,8 @@ package com.peersync;
 import java.io.IOException;
 
 import com.peersync.cli.ShellConsole;
-import com.peersync.data.DataBaseManager;
 import com.peersync.network.PeerSync;
+import com.peersync.network.UpnpManager;
 import com.peersync.tools.Constants;
 
 public class main {
@@ -16,14 +16,15 @@ public class main {
 
 		System.setProperty("net.jxta.endpoint.WireFormatMessageFactory.CBJX_DISABLE", "true");
 		
-		System.setProperty("java.util.logging.config.file", "C:\\PeerSyncTest\\Client1\\log.properties");
+		//System.setProperty("java.util.logging.config.file", "C:\\PeerSyncTest\\Client1\\log.properties");
 //		System.setProperty("net.jxta.logging.Logging", "FINEST");
 //		System.setProperty("net.jxta.level", "FINEST");
 		Constants.getInstance().PEERNAME = "client1";
-		//UpnpManager upnp = UpnpManager.getInstance();
-		//upnp.findGateway();
-		//int port = upnp.openPort(9789, 9789, 9989, "TCP", "PeerSync");
-		Constants.getInstance().PORT = 9788;
+		UpnpManager upnp = UpnpManager.getInstance();
+		upnp.findGateway();
+		int port = upnp.openPort(9788, 9788, 9790, "TCP", "PeerSync");
+		System.out.println(port);
+		Constants.getInstance().PORT = 9799;
 //		
 //		try {
 //			KeyStoreManager.getInstance().exportPeerGroup(Constants.PsePeerGroupID.toString(), "./key", "florian".toCharArray(), KeyStoreManager.MyKeyStorePassword.toCharArray());
--- PeerSync/src/com/peersync/mainClass.java
+++ PeerSync/src/com/peersync/mainClass.java
@@ -1,24 +1,13 @@
 package com.peersync;
 
-import java.io.FileReader;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Security;
+import java.net.InetAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import javax.crypto.Cipher;
-
-import net.jxta.document.AdvertisementFactory;
-import net.jxta.document.MimeMediaType;
-import net.jxta.document.StructuredDocumentFactory;
-import net.jxta.document.XMLDocument;
-import net.jxta.impl.membership.pse.PSEUtils;
-import net.jxta.protocol.PeerGroupAdvertisement;
-
-import com.peersync.network.content.ContentSecurity;
-import com.peersync.tools.Constants;
-import com.peersync.tools.KeyStoreManager;
+import org.wetorrent.upnp.GatewayDevice;
+import org.wetorrent.upnp.GatewayDiscover;
+import org.wetorrent.upnp.LogUtils;
+import org.wetorrent.upnp.PortMappingEntry;
 
 
 
@@ -28,32 +17,59 @@ import com.peersync.tools.KeyStoreManager;
 public class mainClass {
 
 
-	
 
-		  public static void main(String[] unused) throws Exception {
-			  
+
+	public static void main(String[] unused) throws Exception {
+
+		
+		Logger logger = LogUtils.getLogger();
+		logger.info("Starting weupnp");
+
+		GatewayDiscover discover = new GatewayDiscover();
+		logger.info("Looking for Gateway Devices");
+		discover.discover();
+		GatewayDevice d = discover.getValidGateway();
+
+		if (null != d) {
+		    logger.log(Level.INFO, "Gateway device found.\n{0} ({1})", new Object[]{d.getModelName(), d.getModelDescription()});
+		} else {
+		    logger.info("No valid gateway device found.");
+		    return;
+		}
+
+		InetAddress localAddress = d.getLocalAddress();
+		logger.log(Level.INFO, "Using local address: {0}", localAddress);
+		String externalIPAddress = d.getExternalIPAddress();
+		logger.log(Level.INFO, "External address: {0}", externalIPAddress);
+		PortMappingEntry portMapping = new PortMappingEntry();
+
+		logger.log(Level.INFO, "Attempting to map port {0}", 9000);
+		logger.log(Level.INFO, "Querying device to see if mapping for port {0} already exists", 9000);
+
+		if (!d.getSpecificPortMappingEntry(9000,"TCP",portMapping)) {
+		    logger.info("Sending port mapping request");
+
+		    if (d.addPortMapping(9000,9000,localAddress.getHostAddress(),"TCP","test")) {
+		        logger.log(Level.INFO, "Mapping succesful: waiting {0} seconds before removing mapping.", 20);
+		        
+		        Thread.sleep(1000*20);
+		        d.deletePortMapping(9000,"TCP");
+
+		        logger.info("Port mapping removed");
+		        logger.info("Test SUCCESSFUL");
+		    } else {
+		        logger.info("Port mapping removal failed");
+		        logger.info("Test FAILED");
+		    }
 		    
-		    // Generate a key-pair
-		   // KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-		    //kpg.initialize(512); // 512 is the keysize.
-		    //KeyPair kp = kpg.generateKeyPair();
-		    KeyStoreManager ks =  KeyStoreManager.getInstance();
-		    ks.createNewKeys("test", KeyStoreManager.MyKeyStorePassword.toCharArray());
-		    PublicKey pubk = ks.getX509Certificate("test").getPublicKey();
-		    PrivateKey prvk = ks.getPrivateKey("test",KeyStoreManager.MyKeyStorePassword.toCharArray()) ;
+		} else {
+		    logger.info("Port was already mapped. Aborting test.");
+		}
 
-		    byte[] dataBytes =
-		        "J2EE Security for Servlets, EJBs and Web Services".getBytes();
-
-		    byte[] encBytes = ContentSecurity.encrypt(dataBytes, pubk);
-		    byte[] decBytes = ContentSecurity.decrypt(encBytes, prvk);
-
-		    boolean expected = java.util.Arrays.equals(dataBytes, decBytes);
-		    System.out.println("Test " + (expected ? "SUCCEEDED!" : "FAILED!"));
-		  }
-
+		logger.info("Stopping weupnp");
 
 	}
+}
 
 
 
